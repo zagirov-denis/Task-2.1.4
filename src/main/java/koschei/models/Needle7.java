b3dbf2854181c803fac5b7a36e3589a2e28deae4
Denzmc <degtyar87@mail.ru>
2023-05-22 23:29:11 +0500

    Task-2.1.4 (#1)

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,9 +1,18 @@
 package koschei.models;
 
-public class Needle7 {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
+public class Needle7 {
+    private Deth8 deth;
+
+    @Autowired
+    public void setDeth(Deth8 deth) {
+        this.deth = deth;
+    }
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return ", смерть Кощея на игле :( " + deth.toString();
     }
 }
